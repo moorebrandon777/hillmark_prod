ce95127484171be387fd82dd3547cc022d240a66
NephygTdkingz <tdkingzict@gmail.com>
2024-04-02 19:38:53 +0100

    added custome domain to allowed host

--- hillmark_online/settings.py
+++ hillmark_online/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh", 'localhost']
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh", 'localhost', 'www.hmb-online.com', 'hmb-online.com']
 
 
 # Application definition
