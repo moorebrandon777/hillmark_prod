459f6eedec6e045e109c66c7a8d6b4a1cec984f5
NephygTdkingz <tdkingzict@gmail.com>
2025-06-12 14:33:17 +0100

    changed smtp to use tls

--- hillmark_online/settings.py
+++ hillmark_online/settings.py
@@ -154,10 +154,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'mail.privateemail.com'
-EMAIL_PORT = 465
-# EMAIL_PORT = 587
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
+# EMAIL_PORT = 465
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = 'Hillmark <customerservice@hmb-online.com>'
